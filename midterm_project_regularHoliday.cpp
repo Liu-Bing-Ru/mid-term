10a2d259312e655ee7132685c846c1c4e8e6625b
10290429 <110294943+10290429@users.noreply.github.com>
2022-11-08 15:34:45 +0800

    Add files via upload

---
+++ midterm_project_regularHoliday.cpp
@@ -0,0 +1,67 @@
+#include <iostream> 
+#include<cstring>
+#include<cctype>
+using namespace std;
+
+
+int main()
+{
+
+	int workers = 0, days = 0 , shiftNum = 0, holidays = 0, w1 = 0, w2 = 0, requestNum = 0;
+	int shift[50][50] = {0}, needs[50][50] = {0}, holidayRequest[5][1000] = {0};
+	
+	int workTime[100][50] = {0}, workingMember[50][50] = {0}; 
+	
+	int rest[50][50] = {0}, restShift[100] = {0};
+	
+	cin >> workers >> days >> shiftNum >> holidays >> w1 >> w2 >> requestNum;
+	
+	for (int i = 0; i <= shiftNum; i ++)
+		for (int j = 0; j < 24; j ++)
+			cin >> shift[i][j];
+	
+	for (int i = 0; i <= days; i ++)
+		for (int j = 0; j < 24; j ++)
+			cin >> needs[i][j];
+
+	for (int i = 0; i < 2; i ++)
+		for (int j = 0; j < requestNum; j ++)
+			cin >> holidayRequest[i][j];
+			
+	for (int i = 0; i < 7; i ++)
+		for (int j = 0; j < days; j ++)
+			rest[i][j] = 0;
+
+
+	for (int i = 0; i < 7; i ++)
+	{
+		if (i != 6)
+		{
+			for (int j = i; j < days; j = j + 7)
+				rest[i][j] = 1;		
+		}
+		
+		if (i == 6)
+		{
+			for (int k = 0; k < days; k = k + 7)
+				rest[i][k + 6] = 1;	
+		}
+	}
+	
+	
+	for (int i = 0; i < workers; i ++)
+	{
+		for (int j = 0; j < days; j ++)
+		{
+			int k = i % 7;
+			if (rest[k][j] == 1)
+			{
+				workTime[i][j] = shiftNum;
+			}		
+		}
+	}
+	
+	return 0;
+}
+
+  
